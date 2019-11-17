b8a9d9f614dad0a4fa5726b30015695fb08b9022
Valentine Radchuk <valentine.radchuk@outlook.com>
2019-11-18 01:38:03 +0200

    Add index controller, minor improvements

---
+++ hw4/DbManipulator/Controllers/IndexController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbManipulator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbManipulator.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        private readonly MariadbtestContext _dbContext;
+
+        public IndexController(MariadbtestContext  dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result = await _dbContext.Test.CountAsync();
+            return Ok(new {totalRecords = result});
+        }
+    }
+}
