7ffe1c0d27f25459c4d0ccb5df9d8d6cc317900c
Valentine Radchuk <vradchuk@cogniance.com>
2019-12-02 18:50:02 +0200

    Initial commit of db populator

---
+++ hw8/PopulateDb/PopulateDb/Models/Innodb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PopulateDb.Models
+{
+    public partial class Innodb
+    {
+        public uint Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int BirthDay { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthYear { get; set; }
+    }
+}
---
+++ hw8/PopulateDb/PopulateDb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+
+namespace PopulateDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=192.168.2.102; Port=3306;User Id=<login>;Password=<pass>;Database=hw8";
+            MainAsync().GetAwaiter();
+        }
+
+        static async Task MainAsync()
+        {
+        }
+    }
+}
